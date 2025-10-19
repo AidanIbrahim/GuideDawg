b3f185d3ae2dbb25e8ee8f7b55c97c08ccd5d5f8
AidanIbrahim <aidan.ibrahim@outlook.com>
2025-10-19 19:13:31 -0400

    Ignore Python cache and compiled files

--- Pathing/PathingUnitTests.py
+++ Pathing/PathingUnitTests.py
@@ -1,5 +1,5 @@
 """"
-File:    GPS.py
+File:    PathingUnitTests.py
 Author:  Aidan Ibrahim
 Date:    10/18/2025 
 Description:
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 # Constants
 FILE_TYPE = ".graphml"  # File type for saving street maps
 FILE_NAME = "UMBC_StreetMap" + FILE_TYPE    # Filename to save the map as, FILE_TYPE handles the graphml extension.
-MAP = sms.loadGraph("UMBC_StreetMap")
+MAP = None # Global variable to hold the loaded map graph
 
 # Shortcuts for text formatting during testing output
 COLOR_RESET = "\033[0m"
@@ -26,6 +26,8 @@ TEST_FAIlED = COLOR_BOLD_RED + "FAILED" + COLOR_RESET
 
 
 if __name__ == "__main__":
+    print(COLOR_BLUE + "***********************************************************************\n" + COLOR_RESET)
     print(COLOR_BLUE + "Running Pathing Unit Tests...\n" + COLOR_RESET)
+    print(f"Test 1.1: Loading Map... {TEST_PASSED}\n")
     gps.showMap(FILE_NAME)
-    print(f"Test 1.1: Displaying Map... {TEST_PASSED}\n")
+    print(f"Test 1.2: Displaying Map... {TEST_PASSED}\n")
